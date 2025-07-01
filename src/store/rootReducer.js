b9fc1ce256ac5fc755c80d1814aab7b26ca31e66
Livingstone Asabahebwa <lasabahebwa@gmail.com>
2025-07-01 21:53:26 +0300

    refactor the rest of the codebase to use typescript

--- src/store/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import { bulkForecastReducer } from "./bulkForecast/index";
-
-export const rootReducer = combineReducers({
-  bulkForecast: bulkForecastReducer,
-  //   user: userReducer,
-});
